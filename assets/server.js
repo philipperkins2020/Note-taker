3c97f83f3295c9ff63795e1897a34fef0f255a9c
PhilPerk <philipperkins2020@gmail.com>
2021-04-20 14:28:24 -0500

    initial commit

---
+++ assets/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path')
+
+const app = express();
+
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
