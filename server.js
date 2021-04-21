1935de1580e86e4620bc67eb02fcb9c981bda118
PhilPerk <philipperkins2020@gmail.com>
2021-04-21 12:50:01 -0500

    server running

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
 const express = require('express');
 const path = require('path')
-
 const app = express();
-
 var PORT = process.env.PORT || 3000;
-
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
-app.use(express.static('assets'));
+app.use(express.static('public'));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, './index.html')));
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './notes.html')));
+
+const Notes = []
+
+
+app.get('/api/notes', (req, res) => res.json(Notes));
+
+app.post('/api/notes', (req, res) => {
+    
+    const jsonPath = path.join(__dirname, '/db.json')
+    const newNote = req.body;
+  
+
+  });
+
+
+
 
 
 
