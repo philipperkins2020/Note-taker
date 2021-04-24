5411ef277499e4f648c22c561e3c380af3be3d78
PhilPerk <philipperkins2020@gmail.com>
2021-04-24 09:42:43 -0500

    added git ignore

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const util = require('util')
 const path = require('path')
 const fs = require('fs')
+const uuid = require('uuid-random');
 const app = express();
 var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
@@ -10,18 +11,12 @@ app.use(express.static('public'));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, './index.html')));
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './notes.html')));
 
-//const Notes = [ {
-//  NoteTitle:'',
-// NoteText:'',
 
-// }
 
-// ];
-//app.get('/api/notes', (req, res) => res.json(Notes));
 const readFile = util.promisify(fs.readFile)
 app.get('/api/notes', (req, res) => {
-    let db;
-    readFile('db.json', 'utf8').then(notes => {
+    let db = [];
+    readFile('./db.json', 'utf8').then(notes => {
         try {
             db = JSON.parse(notes)
             console.log(db)
@@ -30,30 +25,29 @@ app.get('/api/notes', (req, res) => {
             console.log('error: ' + error)
             db = []
         }
-        //     const NoteTaker = req.params.Notes
-
-        // ;
-        // for (let i = 0; i < Notes.length; i++) {
-        //     if (NoteTaker === Notes[i].NoteTitle,noteText) {
-        //       return res.json(Notes[i]);
-        //     }
-        //   }
-
         return res.json(db);
-
-
     })
-
 });
-
+const writeFileAsync = util.promisify(fs.writeFile);
 app.post('/api/notes', (req, res) => {
-
-    const jsonPath = path.join(__dirname, '/db.json')
-    const newNote = req.body;
-
-
+    readFile('./db.json', 'utf8').then(notes => {
+        const currentNotes = JSON.parse(notes);
+        currentNotes.push({...req.body, id: uuid()});
+        writeFileAsync('./db.json', JSON.stringify(currentNotes));
+        res.json(200);
+    });
 });
 
+app.delete('/api/notes/:id', (req, res) => {
+    readFile('./db.json', 'utf8').then(notes => {
+        let currentNotes = JSON.parse(notes);
+        currentNotes = currentNotes.filter((note)=>{
+            return note.id !== req.params.id
+        });
+        writeFileAsync('./db.json', JSON.stringify(currentNotes));
+        res.json(200);
+    });
+});
 
 
 
